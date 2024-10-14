81334df9fca07b4c451b1b76839d0ed9e2ce393c
GabrielMoreau39 <gabriel.moreau@edu.univ-fcomte.fr>
2024-10-14 10:04:22 +0200

    création header

--- src/App.vue
+++ src/App.vue
@@ -1,22 +1,12 @@
 <script setup lang="ts">
-import { RouterLink, RouterView } from 'vue-router/auto'
-import LogInOut from './components/LogInOut.vue';
+import { RouterView } from 'vue-router/auto'
+import HeaderPage from './components/HeaderPage.vue';
 </script>
 
 <template>
-  <header>
-    <div class="wrapper">
-      <h1 class="text-2xl bg-slate-400">Portfolio</h1>
-      <LogInOut />
-      <nab>
-        <ul>
-          <RouterLink to="/">Accueil</RouterLink>
-          <RouterLink to="/about">About</RouterLink>
-        </ul>
-      </nab>
-    </div>
-  </header>
+  <HeaderPage />
   <main>
     <RouterView />
   </main>
+
 </template>
---
+++ src/components/HeaderPage.vue
@@ -0,0 +1,52 @@
+<script setup lang="ts">
+import { computed, ref } from 'vue'
+import { RouterLink, useRoute } from 'vue-router'
+import IconLogo from './icons/IconLogo.vue';
+
+const route = useRoute()
+const hideOnRoutes = ['']
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+const hide = computed(() => !hideOnRoutes.some(path => route.path.startsWith(path)))
+
+const activeMenu = ref(false)
+
+function closeMenu() {
+    activeMenu.value = false
+}
+</script>
+
+<template>
+    <div class="bg-black items-center justify-between flex px-14">
+        <RouterLink to="/">
+            <IconLogo />
+        </RouterLink>
+        <div>
+            <div>
+                <nav class="invisible opacity-0 flex-1 px-6 py-12 flex justify-between mt-20 flex-col fixed z-10 inset-0 bg-darkRed lg:mt-0 lg:flex-row lg:static lg:bg-transparent lg:py-6 lg:visible lg:opacity-100"
+                    :class="{ '!visible !opacity-100': activeMenu }" v-scroll-lock="activeMenu">
+                    <ul class="flex flex-col gap-4 lg:flex-row">
+
+                        <li>
+                            <RouterLink
+                                :class="{ 'bg-moredarkRed lg:bg-transparent': $route.path === '/About', 'lg:hover:before:border-b-white lg:hover:border-b-1 lg:hover:border-b lg:hover:rounded-none lg:hover:w-full': true }"
+                                class="block p-4 rounded-lg text-white" to="/About" @click="closeMenu">A propos
+                            </RouterLink>
+                        </li>
+                        <li>
+                            <RouterLink
+                                :class="{ 'bg-moredarkRed lg:bg-transparent': $route.path === '/Projets', 'lg:hover:before:border-b-white lg:hover:border-b-1 lg:hover:border-b lg:hover:rounded-none lg:hover:w-full': true }"
+                                class="block p-4 rounded-lg text-white" to="/Projets" @click="closeMenu">Projets
+                            </RouterLink>
+                        </li>
+                        <li>
+                            <RouterLink
+                                :class="{ 'bg-moredarkRed lg:bg-transparent': $route.path === '/Contact', 'lg:hover:before:border-b-white lg:hover:border-b-1 lg:hover:border-b lg:hover:rounded-none lg:hover:w-full': true }"
+                                class="block p-4 rounded-lg text-white" to="/Contact" @click="closeMenu">Contact
+                            </RouterLink>
+                        </li>
+                    </ul>
+                </nav>
+        </div>
+        </div>
+    </div>
+</template>
