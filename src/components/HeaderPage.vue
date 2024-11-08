bd4d3b84ec98620e4045831d1259eb1a0ee3ae5a
GabrielMoreau39 <gabriel.moreau@edu.univ-fcomte.fr>
2024-11-08 08:52:55 +0100

    modifications icones réseaux sociaux menu mobile

--- src/components/HeaderPage.vue
+++ src/components/HeaderPage.vue
@@ -1,33 +1,41 @@
 <script setup lang="ts">
-import { computed, ref } from 'vue'
-import { RouterLink, useRoute } from 'vue-router'
-import IconLogoBlanc from './icons/IconLogoBlanc.vue';
-import IconMoon from './icons/IconMoon.vue';
-import IconSun from './icons/IconSun.vue';
-import { useDark, useToggle } from '@vueuse/core'
-import IconLogoNoir from './icons/IconLogoNoir.vue';
+    import { computed, ref } from 'vue'
+    import { RouterLink, useRoute } from 'vue-router'
+    import IconLogoBlanc from './icons/IconLogoBlanc.vue';
+    import IconMoon from './icons/IconMoon.vue';
+    import IconSun from './icons/IconSun.vue';
+    import { useDark, useToggle } from '@vueuse/core'
+    import IconLogoNoir from './icons/IconLogoNoir.vue';
+    import IconBehance from './icons/IconBehance.vue';
+    import IconInstagram from './icons/IconInstagram.vue';
+    import IconLinkedin from './icons/IconLinkedIn.vue';
+    import IconGitHub from './icons/IconGitHub.vue';
+    import IconLikedInNoir from './icons/IconLikedInNoir.vue';
+    import IconBehanceNoir from './icons/IconBehanceNoir.vue';
+    import IconInstagramNoir from './icons/IconInstagramNoir.vue';
+    import IconGithubNoir from './icons/IconGithubNoir.vue';
 
-const route = useRoute()
-const hideOnRoutes = ['']
-// eslint-disable-next-line @typescript-eslint/no-unused-vars
-const hide = computed(() => !hideOnRoutes.some(path => route.path.startsWith(path)))
+    const route = useRoute()
+    const hideOnRoutes = ['']
+    // eslint-disable-next-line @typescript-eslint/no-unused-vars
+    const hide = computed(() => !hideOnRoutes.some(path => route.path.startsWith(path)))
 
-const activeMenu = ref(false)
+    const activeMenu = ref(false)
 
-function closeMenu() {
-    activeMenu.value = false
-}
-const isDark = useDark({
-    selector: 'html',
-    attribute: 'class',
-    valueDark: 'dark',
-    valueLight: 'light',
-})   
-const toggleDark = useToggle(isDark)
+    function closeMenu() {
+        activeMenu.value = false
+    }
+    const isDark = useDark({
+        selector: 'html',
+        attribute: 'class',
+        valueDark: 'dark',
+        valueLight: 'light',
+    })   
+    const toggleDark = useToggle(isDark)
 </script>
 
 <template>
-    <div class="bg-black dark:bg-white items-center justify-between flex px-4 lg:px-14 py-5 lg:py-0">
+    <div class="bg-black dark:bg-white items-center justify-between flex px-4 lg:px-14 py-5 lg:py-0 px-6">
         <div>
             <RouterLink to="/fr/">
                 <IconLogoBlanc v-show="!isDark" class="w-16 h-auto lg:w-24" />
@@ -49,15 +57,15 @@ const toggleDark = useToggle(isDark)
                     <span :class="{ '!opacity-0': activeMenu }"></span>
                     <span :class="{ '-translate-y-[7px] -rotate-45 !w-7': activeMenu }"></span>
                 </button>
-                <nav class="invisible opacity-0 flex-1 px-6 py-12 flex justify-between mt-20 flex-col fixed z-10 inset-0 bg-black dark:bg-white lg:mt-0 lg:flex-row lg:static lg:bg-transparent lg:py-6 lg:visible lg:opacity-100"
+                <nav class="invisible opacity-0 flex-1 py-12 flex justify-between mt-20 flex-col fixed z-10 inset-0 bg-black dark:bg-white lg:mt-0 lg:flex-row lg:static lg:bg-transparent lg:py-6 lg:visible lg:opacity-100"
                     :class="{ '!visible !opacity-100': activeMenu }" v-scroll-lock="activeMenu">
-                    <ul class="flex flex-col gap-4 lg:flex-row">
+                    <ul class="flex flex-col gap-4 lg:flex-row px-6">
 
                         <li>
                             <RouterLink
                                 :class="{ 'bg-moredarkRed lg:bg-transparent': $route.path === '/fr/about', 'lg:hover:before:border-b-white lg:dark:hover:before:border-b-black lg:hover:border-b-1 lg:hover:border-b lg:hover:rounded-none lg:hover:w-full': true }"
                                 class="block p-4 rounded-lg text-white dark:text-black" to="/fr/about"
-                                @click="closeMenu">A propos
+                                @click="closeMenu">À propos
                             </RouterLink>
                         </li>
                         <li>
@@ -75,19 +83,34 @@ const toggleDark = useToggle(isDark)
                             </RouterLink>
                         </li>
                     </ul>
-                    <div class="flex flex-rox justify-around items-center">
+                    <div class="flex flex-row justify-center items-center space-x-48">
                         <div class="flex flex-col">
-                            <div class="flex flex-row"></div>
-                            <div class="flex flex-row"></div>
+                            <div class="flex grid-cols-2 grid-rows-2 space-x-2">
+                                <div v-show="!isDark" class="space-y-2">
+                                    <IconLinkedin />
+                                    <IconBehance />
+                                </div>
+                                <div v-show="!isDark" class="space-y-2">
+                                    <IconInstagram />
+                                    <IconGitHub />
+                                </div>
+                                <div v-show="isDark" class="space-y-2">
+                                    <IconLikedInNoir />
+                                    <IconBehanceNoir />
+                                </div>
+                                <div v-show="isDark" class="space-y-2">
+                                    <IconInstagramNoir />
+                                    <IconGithubNoir />
+                                </div>
+                            </div>
                         </div>
-                        <div class="flex flex-row lg:hidden text-white">
+                        <div class="flex flex-row lg:hidden text-white dark:text-black">
                             <RouterLink to="/fr/">
                                 <strong>FR</strong>/EN
                             </RouterLink>
                         </div>
                     </div>
                 </nav>
-                
             </div>
         </div>
     </div>
