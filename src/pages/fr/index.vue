bd14e29c9b0fc73d028f946e8edc5d531fa20f91
GabrielMoreau39 <gabriel.moreau@edu.univ-fcomte.fr>
2024-11-10 11:38:05 +0100

    test ajout background section 1 page d'accueil

--- src/pages/fr/index.vue
+++ src/pages/fr/index.vue
@@ -5,16 +5,16 @@
 
 <template>
     <div class="bg-black dark:bg-white text-white dark:text-black">
-        <section class="space-y-32 py-32">
+        <section class="space-y-28 py-28 bg-[url(img/grille-background.svg)] bg-no-repeat bg-cover">
             <div class="text-center space-y-6">
-                <h2 class="font-body text-3xl">g a b r i e l</h2>
-                <h2 class="font-body text-3xl">m o r e a u</h2>
-                <h3 class="font-body text-xl pt-2">Développeur Web</h3>
+                <h2 class="font-body text-3xl dark:text-black">g a b r i e l</h2>
+                <h2 class="font-body text-3xl dark:text-black">m o r e a u</h2>
+                <h3 class="font-body text-xl pt-2 dark:text-black">Développeur Web</h3>
             </div>
             <div class="display flex flex-row justify-center items-center space-x-2">
                 <IconPlus />
-                <p class="text-base">En savoir plus</p>
+                <p class="text-base dark:text-black">En savoir plus</p>
             </div>
         </section>
     </div>
-</template>
+</template> 
