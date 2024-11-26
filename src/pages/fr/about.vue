a20ff594c60b4890771c3c37bed9f58f6e4f5c6b
GabrielMoreau39 <gabriel.moreau@edu.univ-fcomte.fr>
2024-11-27 00:34:32 +0100

    ajout première section page about

--- src/pages/fr/about.vue
+++ src/pages/fr/about.vue
@@ -4,7 +4,13 @@
 </script>
 
 <template>
-    <div>
-      <h1 class="text-2xl bg-slate-400">About</h1>
-    </div>  
+  <div class="bg-black dark:bg-white">
+    <section class="px-7 py-10 space-y-6">
+      <h2 class="dark:text-black">Qui suis-je ?</h2>
+      <p>Bonjour, je m'appelle Gabriel Moreau, j'ai 18 ans et je suis actuellement en deuxième année de BUT MMI (Métiers
+        du Multimédia et de l'Internet) à Montbéliard. Je me suis spécialisé en Développement Web. À travers ce site,
+        vous aurez l'occasion de découvrir mes différents travaux/projets qui vous permettront de voir mes compétences
+        et les outils/applications que je maîtrise.</p>
+    </section>
+  </div>
 </template>
