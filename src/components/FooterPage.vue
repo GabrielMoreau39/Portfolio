e83f359243fa685fed95362fb311f03837630c15
GabrielMoreau39 <gabriel.moreau@edu.univ-fcomte.fr>
2024-12-18 16:13:14 +0100

    mise à jour du lien de téléchargement du CV et amélioration de la structure du footer

--- src/components/FooterPage.vue
+++ src/components/FooterPage.vue
@@ -30,19 +30,35 @@
             </RouterLink>
         </div>
         <div>
-            <a href="/CV_Gabriel-Moreau.pdf" download="CV_Gabriel-Moreau.pdf"
+            <a href="/CV_Développeur-Web.pdf" download="CV_Gabriel-Moreau.pdf"
                 class="dark:border-b-black border-b-white border-b-2 text-white dark:text-black">Télécharger mon
                 CV</a>
         </div>
         <div class="display flex justify-center items-center space-x-4">
-            <a href="https://www.linkedin.com"><IconLinkedIn v-show="!isDark" /></a>
-            <a href="https://www.linkedin.com"><IconLikedInNoir v-show="isDark" /></a>
-            <a href="https://www.behance.net"><IconBehance v-show="!isDark" /></a>
-            <a href="https://www.behance.net"><IconBehanceNoir v-show="isDark" /></a>
-            <a href="https://www.instagram.com"><IconInstagram v-show="!isDark" /></a>
-            <a href="https://www.instagram.com"><IconInstagramNoir v-show="isDark" /></a>
-            <a href="https://github.com"><IconGitHub v-show="!isDark" /></a>
-            <a href="https://github.com"><IconGithubNoir v-show="isDark" /></a>
+            <a href="https://www.linkedin.com">
+                <IconLinkedIn v-show="!isDark" />
+            </a>
+            <a href="https://www.linkedin.com">
+                <IconLikedInNoir v-show="isDark" />
+            </a>
+            <a href="https://www.behance.net">
+                <IconBehance v-show="!isDark" />
+            </a>
+            <a href="https://www.behance.net">
+                <IconBehanceNoir v-show="isDark" />
+            </a>
+            <a href="https://www.instagram.com">
+                <IconInstagram v-show="!isDark" />
+            </a>
+            <a href="https://www.instagram.com">
+                <IconInstagramNoir v-show="isDark" />
+            </a>
+            <a href="https://github.com">
+                <IconGitHub v-show="!isDark" />
+            </a>
+            <a href="https://github.com">
+                <IconGithubNoir v-show="isDark" />
+            </a>
         </div>
         <p class="text-sm pt-2 dark:text-black">© 2024 Gabriel Moreau</p>
     </div>
